94cf25f55caac749aa3a54c10e621d20c451fb87
softyAmir <theyabcpssouaf2@gmail.com>
2023-11-05 09:36:53 -1100

    post,get order Route commit

--- controllers/ordersCrud.js
+++ controllers/ordersCrud.js
@@ -1,26 +1,45 @@
 const Order = require('../schema/orders');
 
+const Customer = require('../schema/customers'); // Import the Customer model
+const Product = require('../schema/product'); // Import the Product model
+
+const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate({
+        path: 'customer',
+        model: Customer,
+        select: 'firstName lastName email profileImage',
+      })
+      .populate({
+        path: 'products.product', 
+        model: Product,
+        select: 'name price quantity', 
+      });
+
+    res.json({ success: true, orders });
+  } catch (err) {
+    console.error('Error fetching orders:', err);
+    res.status(500).json({ success: false, error: 'Error fetching orders' });
+  }
+};
 //getAllPosts
 const postOrders = async (req, res) => {
     try {
-      // Retrieve the order data from the request body
+      
       const orderData = req.body;
   
-      // Create a new order in your database (e.g., MongoDB)
-      const newOrder = await Order.create(orderData); // Assuming you have an Order model
+      
+      const newOrder = await Order.create(orderData);
   
-      // Respond with a success message and the newly created order
+      
       res.status(201).json({ message: 'Order created successfully', order: newOrder });
     } catch (error) {
-      // Handle any errors that may occur during order creation
+      
       console.error('Error creating order:', error);
       res.status(500).json({ message: 'Internal server error' });
     }
   };
-
-const getOrders = async (req, res) => {
-
-};
 
 
 module.exports = {
